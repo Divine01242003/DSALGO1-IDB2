3e3a917d020ca74092b41df70e4fd3c631606d70
mahusay_di <mahusay_di@f213.ub.lan>
2024-08-30 15:58:23 +0800

    including my submission for Exercise 1

---
+++ Exercise 1/Exer.py
@@ -0,0 +1,51 @@
+'''
+userInput = int (input("How much money do you have? "))
+wii=100
+afford=int (userInput/wii)
+neededMoney = abs ((userInput % wii) - wii)
+
+
+print("You",userInput,"Pesos")
+print("You can afford",afford,"Wii's")
+print("You needed ",neededMoney,"pesos to afford a wii's")
+'''
+'''
+sum = 0 
+for x in range (1,11):
+    sum = sum + x
+    print("This is the sum of the numbers",sum)
+
+'''
+'''
+Input1 = int(input("Enter first Numbers: "))
+Input2 = int(input("Enter second Number: "))
+sum = 0
+
+for x in range(Input1,Input2):
+    sum = sum + x
+print("The ",sum," of the Inputed numbers of",Input1,"and",Input2)
+
+'''
+'''
+userInput = int(input("Enter a number:"))
+factorial = 1
+
+for x in range(1, userInput+1):
+    factorial = factorial * x
+print("The factorial of ", x, "is", factorial)
+'''
+'''
+userInput = int(input("Enter a number: "))
+print("The factors of your number are: ")
+for x in range(1, userInput + 1):
+        if userInput % x == 0:
+         print(x)
+'''
+
+factorList = []
+x =int (input("Enter a Number"))
+for x in range(1, userInput + 1):
+        if userInput % x == 0:
+         factorList.append(a)
+         print("The factorial of", x,"are,")
+
